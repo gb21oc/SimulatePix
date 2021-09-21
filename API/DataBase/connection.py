8ba658108f7d4c07834dbec50238ca2dac18290f
Gabriel Jose <luckygb21@gmail.com>
2021-09-20 23:28:38 -0300

    mudança na connectionString

--- API/DataBase/connection.py
+++ API/DataBase/connection.py
@@ -1,6 +1,6 @@
 import pymongo
 
-connectionString = ""
+connectionString = "<your_connectionString>"
 mongodbClient = pymongo.MongoClient(connectionString, serverSelectionTimeoutMS=5000)
 database = mongodbClient['SimulatePix']
 dbAccount = database['Account']
