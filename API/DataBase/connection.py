e50dccb6648c6b03817908c1d56c9bc467d7e63b
Gabriel Jose <luckygb21@gmail.com>
2021-09-20 22:59:28 -0300

    Test GitIgnore

--- API/DataBase/connection.py
+++ API/DataBase/connection.py
@@ -1,6 +1,6 @@
 import pymongo
 
-connectionString = ""
+connectionString = "A"
 mongodbClient = pymongo.MongoClient(connectionString, serverSelectionTimeoutMS=5000)
 database = mongodbClient['SimulatePix']
 dbAccount = database['Account']
